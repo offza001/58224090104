b2fafed9cd7c5cde7f7bfbd1e6d6640034ff3dcb
offza001 <31381757+offza001@users.noreply.github.com>
2017-09-03 15:55:41 +0700

    Add 3-13.c

---
+++ 3-13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int number = 25;
+	int a = 1, b = 2, c = 3;
+	float real = 99.99;
+	float pointl=45.2, point2=30;
+	char choice = 'a';
+	char ch1='0',ch2='z';
+	printf("number = %d\n",number);
+	printf("a=%d , b=%d , c=%d\n",a,b,c);
+	printf("real = %.2f\n",real);
+	printf("pointl=%.1f,point2=%.0f",pointl,point2);
+	printf("choice=%c\n",choice);
+	printf("ch1=%c ch2=%c",ch1,ch2);
+	
+}
