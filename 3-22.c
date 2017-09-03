7394a4f8edde86fd0fc02565d3ae2afedd970c30
offza001 <31381757+offza001@users.noreply.github.com>
2017-09-03 16:41:35 +0700

    add3-22

---
+++ 3-22.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	char subject[12] = "programming";
+	char nick[4] ="com";
+	char nick_1[4] ={'c','o','m','\0'};
+	char name[] ="Jirasak";
+	printf("%15s%15s%15s%15s",subject,nick,nick,name);
+}
